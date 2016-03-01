0616f592dec99f78f3ad59b4fc4c18ea9dfa0d79
Lynn K. Patricia <lkpatricia@gmail.com>
2016-02-29 22:36:23 -0600

    initial load of files

---
+++ random.js
@@ -0,0 +1,11 @@
+
+function queryUser() {
+	alert("I can generate a random integer on a range defined by you.");   
+	var lowerLimit = parseInt(prompt("what is the LOWEST integer of the range?"));  
+	var upperLimit = parseInt(prompt("what is the HIGHEST integer of the range?"));   
+
+	var numberRandom = Math.floor(Math.random() * (upperLimit - lowerLimit + 1)) + lowerLimit;
+	
+	document.getElementById("numberRange").innerHTML = "Here is a really nice random integer on a range from " + lowerLimit + " to " + upperLimit + ":";
+	document.getElementById("numberRandom").innerHTML = numberRandom;
+}
