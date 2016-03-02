79d2250c456bed8085484d2becb261198757fde1
Lynn K. Patricia <lkpatricia@gmail.com>
2016-03-01 22:16:18 -0600

    fix sentence grammar

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
 
 function queryUser() {
 	alert("I can generate a random integer on a range defined by you.");   
-	var lowerLimit = parseInt(prompt("what is the LOWEST integer of the range?"));  
-	var upperLimit = parseInt(prompt("what is the HIGHEST integer of the range?"));   
+	var lowerLimit = parseInt(prompt("What is the LOWEST integer of the range?"));  
+	var upperLimit = parseInt(prompt("What is the HIGHEST integer of the range?"));   
 
 	var numberRandom = Math.floor(Math.random() * (upperLimit - lowerLimit + 1)) + lowerLimit;
 	
